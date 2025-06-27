e7bbf64e1bab5de26755e79bfb5236b5a5799f01
_Avvake_ <denisergo18@gmail.com>
2025-06-27 16:44:18 +0300

    added the user card info

--- srm_front/src/components/Main/Main.tsx
+++ srm_front/src/components/Main/Main.tsx
@@ -9,7 +9,7 @@ function Main() {
         <main className="flex-1 p-6 bg-gray-400">
             <h1 className="text-center font-bold text-3xl">SRM</h1>
             {showFilter && <FilterItem />}
-            <div className="container p-2">
+            <div className="container px-2">
                 <Outlet />
             </div>
         </main>
