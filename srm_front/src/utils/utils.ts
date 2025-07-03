39db20224399666ad88b67279a0ba71625f7e10e
_Avvake_ <denisergo18@gmail.com>
2025-07-03 20:52:51 +0300

    function for converting text

---
+++ srm_front/src/utils/utils.ts
@@ -0,0 +1,44 @@
+export function getChangedFields<T>(original: T, updated: T): Partial<T> {
+    const changed: Partial<T> = {};
+
+    for (const key in original) {
+        if (updated[key] !== original[key])
+            changed[key] = updated[key];
+    }
+    return changed;
+}
+
+
+export function camelToSnake(text: string): string {
+    return text.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+}
+
+export function snakeToCamel(text: string): string {
+    return text.replace(/(_\w)/g, letter => letter[1].toUpperCase());
+}
+
+export function transformKeysToCamelCase<T extends Record<string, any>>(
+  obj: T
+): Record<string, any> {
+  const result: Record<string, any> = {};
+
+  for (const key in obj) {
+    const camelKey = snakeToCamel(key);
+    result[camelKey] = obj[key];
+  }
+
+  return result;
+}
+
+export function transformKeysToSnakeCase<T extends Record<string, any>>(
+  obj: T
+): Record<string, any> {
+  const result: Record<string, any> = {};
+
+  for (const key in obj) {
+    const snakeKey = camelToSnake(key);
+    result[snakeKey] = obj[key];
+  }
+
+  return result;
+}
