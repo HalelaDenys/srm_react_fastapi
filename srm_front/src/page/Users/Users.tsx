407326d85afb9c02665df274dd79002137be59c5
_Avvake_ <denisergo18@gmail.com>
2025-07-03 21:03:36 +0300

    extracted unneeded from file

--- srm_front/src/page/Users/Users.tsx
+++ srm_front/src/page/Users/Users.tsx
@@ -1,79 +1,65 @@
-import { useState } from "react";
-import { get_users, delete_user, create_user } from "../../api/users";
+import { useCreateUser } from "../../hooks/userHooks/useCreateUser";
+import { useDeleteUser } from "../../hooks/userHooks/useDeleteUser";
 import ContentItem from "../../components/ContentItem/ContentItem";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import UserForm from "../../components/Form/UserForm/UserForm";
+import type { IUserCreateFormData } from "../../entities/user.types";
 import ModalWnd from "../../components/ModalWnd/ModalWnd";
-import UserForm from "../../components/From/UserForm/UserForm";
+import { useUsers } from "../../hooks/userHooks/useUsers";
+import { useState } from "react";
 
 function Users() {
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-    const queryClient = useQueryClient();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data: userData = [], isLoading, error } = useUsers();
+  const createUserMutation = useCreateUser();
+  const deleteUserMutation = useDeleteUser();
 
-    const { isLoading, data: userData = [], error } = useQuery({
-        queryKey: ["users"],
-        queryFn: get_users,
-    })
+  const handleDelete = (id: number) => {
+    deleteUserMutation.mutate(id);
+  };
 
-    const deleteMutation = useMutation({
-        mutationFn: delete_user,
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ["users"] });
-        },
-    });
+  const handleSubmit = (data: IUserCreateFormData) => {
+    createUserMutation.mutate(data);
+    setIsModalOpen(false);
+  };
 
-    const handleDelete = (id: number) => {
-        deleteMutation.mutate(id);
-    };
+  if (isLoading) return "Loading...";
 
-    const createMutation = useMutation({
-        mutationFn: create_user,
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ["users"] });
-        },
-    });
+  if (error) return "An error has occurred: " + error.message;
 
+  if (userData.length === 0) {
+    return <div className="text-center">No users found.</div>;
+  }
 
-    const handleCreateUser =  (data: { firstName: string; lastName: string; phoneNumber: string }) => {
-        createMutation.mutate(data);
-        setIsModalOpen(false);
-    }
-
-    if (isLoading) return "Loading..."
-
-    if (error) return 'An error has occurred: ' + error.message
-
-    if (userData.length === 0) {``
-        return <div className="text-center">No users found.</div>;
-    }
-
-    return (
-        <div>
-            <div className="flex justify-center">
-                <button className="mb-1 p-1.5 text-white hover:text-purple-700
+  return (
+    <div>
+      <div className="flex justify-center">
+        <button
+          className="mb-1 p-1.5 text-white hover:text-purple-700
                 transition duration-300 ease-in-out"
-                    onClick={() => setIsModalOpen(true)}>Create New User
-                </button>
-
-            </div>
-            <div className="flex flex-col gap-2">
-                {userData.map((user) => (
-                    <ContentItem
-                        key={user.id}
-                        id={user.id}
-                        title={user.firstName + " " + user.lastName}
-                        endpoint="users"
-                        onDelete={handleDelete}
-                    />
-                ))}
-            </div>
-            <ModalWnd
-            isOpen={isModalOpen}
-            onClose={() => setIsModalOpen(false)}
-            title="Create New User"
-            content={<UserForm onSubmit={handleCreateUser}/>}
-            />
-        </div>
-    );
+          onClick={() => setIsModalOpen(true)}
+        >
+          Create New User
+        </button>
+      </div>
+      <div className="flex flex-col gap-2">
+        {userData.map((user) => (
+          <ContentItem
+            key={user.id}
+            id={user.id}
+            title={user.firstName + " " + user.lastName}
+            endpoint="users"
+            onDelete={handleDelete}
+          />
+        ))}
+      </div>
+      <ModalWnd
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Create New User"
+        content={<UserForm onSubmit={handleSubmit} />}
+      />
+    </div>
+  );
 }
 
 export default Users;
