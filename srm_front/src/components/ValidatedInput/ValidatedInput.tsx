d63c5ecbc88e9c8a7059b3c32594cfcf45f10482
_Avvake_ <denisergo18@gmail.com>
2025-07-08 14:43:27 +0300

    Added the component "Validated Input"

---
+++ srm_front/src/components/ValidatedInput/ValidatedInput.tsx
@@ -0,0 +1,48 @@
+interface ValidatedInputProps {
+  label: string;
+  name: string;
+  value?: string | number;
+  type?: string;
+  error?: string;
+  onChange: (value: string) => void;
+  checked?: boolean; // для checkbox
+}
+
+function ValidatedInput({
+  label,
+  name,
+  value,
+  type = "text",
+  error,
+  onChange,
+  checked,
+}: ValidatedInputProps) {
+  return (
+    <>
+      <label>{label}</label>
+      {type === "checkbox" ? (
+        <input
+          id={name}
+          name={name}
+          type="checkbox"
+          checked={checked}
+          onChange={(e) => onChange(e.target.checked ? "true" : "false")}
+          className="mb-3 p-1 focus:outline-none focus:border focus:rounded"
+        />
+      ) : (
+        <input
+          id={name}
+          name={name}
+          type={type}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          className="mb-3 p-1 focus:outline-none focus:border focus:rounded"
+        />
+      )}
+
+      {error && <p className="text-red-500">{error}</p>}
+    </>
+  );
+}
+
+export default ValidatedInput;
