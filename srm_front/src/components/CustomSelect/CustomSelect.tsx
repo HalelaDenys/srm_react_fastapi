7c8542b05ffa3477bbdcac65fd8e1edbaa5bb513
_Avvake_ <denisergo18@gmail.com>
2025-07-31 11:54:28 +0300

    create custom select

---
+++ srm_front/src/components/CustomSelect/CustomSelect.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useRef, useState } from "react";
+
+interface ICustomSelect {
+  label: string;
+  selectedValue: string | number;
+  options: { value: string; id: number }[];
+  onChange?: (value: any) => void;
+}
+
+function CustomSelect({
+  label,
+  options,
+  selectedValue,
+  onChange,
+}: ICustomSelect) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const selected =
+    options.find((opt) => opt.value === selectedValue) || options[0];
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+
+  return (
+    <div className="flex flex-row gap-2 mx-1 items-center">
+      <label className="text-lg font-medium italic">{label}</label>
+      <div className="relative w-40" ref={ref}>
+        <div
+          onClick={() => setIsOpen(!isOpen)}
+          className=" border p-2 rounded cursor-pointer"
+        >
+          <div className="flex justify-between items-center">
+            {selected?.value}
+            <img src="/down_icon.png" alt="down icon" className="w-4 h-4"/>
+          </div>
+        </div>
+        {isOpen && (
+          <ul className="absolute top-full left-0 w-full 
+          bg-gray-300 shadow-md z-10 border rounded">
+            {options.map((option) => (
+              <li
+                key={option.id}
+                onClick={() => {
+                  {
+                    onChange && onChange(option);
+                  }
+                  setIsOpen(false);
+                }}
+                className="p-2 hover:bg-gray-100 cursor-pointer"
+              >
+                {option.value}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default CustomSelect;
