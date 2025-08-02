21911a0db9db72a758c357f696619c41ce1016eb
_Avvake_ <denisergo18@gmail.com>
2025-08-02 17:19:40 +0300

    changes in CSS

--- srm_front/src/components/Form/UserForm/UserForm.tsx
+++ srm_front/src/components/Form/UserForm/UserForm.tsx
@@ -1,68 +1,62 @@
 import { useState } from "react";
 import type { IUserFormProps } from "../../../entities/user.types";
-
+import style from "../Form.module.css";
 
 export default function UserForm({ onSubmit }: IUserFormProps) {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [error, setError] = useState<string | null>(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        setError(null);
-        if (!phoneNumber.startsWith('+380')) {
-            setError("Номер телефону повинен починатися з +380");
-            return;
-        }
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(null);
+    if (!phoneNumber.startsWith("+380")) {
+      setError("Номер телефону повинен починатися з +380");
+      return;
+    }
 
-        if (phoneNumber.length !== 13) {
-            setError("Номер телефону повинен містити рівно 13 символів");
-            return;
-        }
+    if (phoneNumber.length !== 13) {
+      setError("Номер телефону повинен містити рівно 13 символів");
+      return;
+    }
 
-        if (!/^\+380\d{9}$/.test(phoneNumber)) {
-            setError("Номер повинен містити лише цифри після +380");
-            return;
-        }
-        onSubmit({ firstName, lastName, phoneNumber });
-    };
+    if (!/^\+380\d{9}$/.test(phoneNumber)) {
+      setError("Номер повинен містити лише цифри після +380");
+      return;
+    }
+    onSubmit({ firstName, lastName, phoneNumber });
+  };
 
-
-
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <div className="flex gap-4">
-                <input
-                    type="text"
-                    placeholder="First Name"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    className="w-full border mb-3 p-2 rounded focus:outline-none focus:border-2"
-                />
-                <input
-                    type="text"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                    placeholder="Last Name"
-                    className="w-full border mb-3 p-2 rounded focus:outline-none focus:border-2"
-                />
-            </div>
-            <input
-                type="text"
-                placeholder="+380 XX XXX XXXX"
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-                className="w-full border mb-4 p-2 rounded focus:outline-none focus:border-2 "
-            />
-            {error && <p className="text-red-500 text-sm">{error}</p>}
-            <button
-                type="submit"
-                className="px-4 py-2 bg-purple-600 text-white rounded hover:bg-purple-700"
-            >
-                Create
-            </button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="flex gap-4">
+        <input
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          className={style["input"]}
+        />
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          placeholder="Last Name"
+          className={style["input"]}
+        />
+      </div>
+      <input
+        type="text"
+        placeholder="+380 XX XXX XXXX"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+        className={style["input"]}
+      />
+      {error && <p className="text-red-500 text-sm">{error}</p>}
+      <button type="submit" className={style["btn_submit"]}>
+        Create
+      </button>
+    </form>
+  );
 }
